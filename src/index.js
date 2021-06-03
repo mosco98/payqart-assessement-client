68a831839fff9ba441bf7e7948a5b1a271755bde
mosco98 <moscolyno98@gmail.com>
2021-06-03 20:52:44 +0100

    Removed React-router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Switch } from "react-router-dom";
+
 import "./assets/scss/styles.css";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <App />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
